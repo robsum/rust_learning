ccfb2a7e2b1a97da28a27b431d9733965547037c
robsum <rob93pl@gmail.com>
2023-02-02 19:31:36 +0100

    variables chapter completed

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -104,6 +104,69 @@ fn main() {
     println!("The value of y is {y}");
 
     println!("The return value of another_function(1) is {}", another_function(1));
+
+    let number = 3;
+    if number < 5 {
+        println!("The condition was true");
+    } else {
+        println!("The condition was false");
+    }
+
+    //let number = if true { 5 } else { "six" };
+    //println("Number is {number}");
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
 
 fn another_function(x: i32) -> i32 {
